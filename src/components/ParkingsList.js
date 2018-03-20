a5db1449c600b0bdaafc2c78da930e6fd7e5beec
ab1hi2shek <ab1hi2shek@gmail.com>
2018-03-21 03:33:21 +0530

    enhanced UI solved bug 3

--- src/components/ParkingsList.js
+++ src/components/ParkingsList.js
@@ -72,9 +72,8 @@ class ParkingList extends Component {
           			<ParkingsMap 
                         markers = {this.props.parkings}
                         isMarkerShown
-                        googleMapURL="https://maps.googleapis.com/maps/api/js?
-                            key=AIzaSyAuqEdlOBNzTgc9QeILQCefKqTljgvQnbw
-                            &v=3.exp&libraries=geometry,drawing,places"
+                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDxP2D4qnJZnsfwCFu9vjhw9vf6qWBRc5k&callback=initMap"
+    async defer
                         loadingElement={<div style={{ height: `100%` }} />}
                         containerElement={<div style={{ height: `550px` }} />}
                         mapElement={<div style={{ height: `100%` }} />}
